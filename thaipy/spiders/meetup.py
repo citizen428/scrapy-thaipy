a96d625fd1f26052c0e6e6f84eeece56b93945d8
Michael Kohl <citizen428@gmail.com>
2017-12-13 22:00:35 +0700

    First version

--- thaipy/spiders/meetup.py
+++ thaipy/spiders/meetup.py
@@ -5,7 +5,12 @@ import scrapy
 class MeetupSpider(scrapy.Spider):
     name = 'meetup'
     allowed_domains = ['meetup.com']
-    start_urls = ['http://meetup.com/']
+    start_urls = [
+        'https://www.meetup.com/ThaiPy-Bangkok-Python-Meetup/events/past']
 
     def parse(self, response):
-        pass
+        for event in response.css('li.list-item'):
+            yield {
+                "title": event.css('h2.eventCardHead--title::text').extract_first(),
+                "date": event.css('.eventTimeDisplay-startDate > span::text').extract()
+            }
