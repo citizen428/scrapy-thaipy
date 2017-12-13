40f8b5dbce3c0c07191d04094a7e587411354ea5
Michael Kohl <citizen428@gmail.com>
2017-12-13 22:31:44 +0700

    Crawlspider

--- thaipy/spiders/meetup.py
+++ thaipy/spiders/meetup.py
@@ -1,18 +1,25 @@
 # -*- coding: utf-8 -*-
 import scrapy
+
+from scrapy.linkextractors import LinkExtractor
 from scrapy.loader import ItemLoader
+from scrapy.spiders import CrawlSpider, Rule
 from thaipy.items import MeetupItem
 
 
-class MeetupSpider(scrapy.Spider):
+class MeetupSpider(CrawlSpider):
     name = 'meetup'
     allowed_domains = ['meetup.com']
     start_urls = [
         'https://www.meetup.com/ThaiPy-Bangkok-Python-Meetup/events/past']
 
-    def parse(self, response):
-        for event in response.css('li.list-item'):
-            loader = ItemLoader(item=MeetupItem(), selector=event)
-            loader.add_css('title', 'h2.eventCardHead--title::text')
-            loader.add_css('date', '.eventTimeDisplay-startDate > span::text')
-            yield loader.load_item()
+    rules = [
+        Rule(LinkExtractor(allow=('events/\w+/')), callback='parse_meetup')
+    ]
+
+    def parse_meetup(self, response):
+        loader = ItemLoader(item=MeetupItem(), response=response)
+        loader.add_css('title', 'h1.pageHead-headline::text')
+        loader.add_css('date', '.eventTimeDisplay-startDate > span::text')
+        loader.add_css('date', '.eventTimeDisplay-startDate-time > span::text')
+        yield loader.load_item()
